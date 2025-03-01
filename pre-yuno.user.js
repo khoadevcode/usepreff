12e89c0b1fb55a7474df40f5ddc57f8341d9089e
khoadevcode <nguyenkienfake@gmail.com>
2025-03-01 22:37:00 +0700

    Create pre-yuno.user.js

---
+++ pre-yuno.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Bypass Ez
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Bypass so cool ( pre - Yuno )
+// @author       UwU
+// @match        *://trigonevo.fun/*
+// @match        *://discord.com/channels/*
+// @match        *://linkvertise.com/*
+// @match        https://loot-link.com/*
+// @match        https://loot-links.com/*
+// @match        https://lootdest.org/*
+// @match        https://lootdest.com/*
+// @match        https://socialwolvez.com/*
+// @match        https://key.fluxteam.org/*
+// @match        https://getzorara.online:2053/*
+// @match        https://key.getwave.gg/*
+// @match        https://pandadevelopment.net/*
+// @match        https://flux.li/*
+// @match        https://spdmteam.com/*
+// @match        https://keyguardian.org/*
+// @match        https://getswift.xyz/real/*
+// @match        https://mobile.codex.lol/*
+// @match        https://ads.luarmor.net/*
+// @match        https://keyrblx.com/*
+// @match        https://rekonise.com/*
+// @match        https://1shortlink.com/*
+// @match        https://bstlar.com/*
+// @match        *://getkey.farrghii.com/*
+// @require      
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_notification
+// @run-at       document-start
+// @icon         https://cdn.discordapp.com/avatars/1248562467240542208/a15472d7a7c67389033a031fc62e98a2.png?size=4096
+// ==/UserScript==
